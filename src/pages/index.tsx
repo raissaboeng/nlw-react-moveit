0ca351b8b975bd369b03957e6df3a230b5c8e7bb
raissaboeng <raissa.boeng@gmail.com>
2021-02-24 19:37:14 -0300

    add component CompletedChallenge and CountDown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { CountDown } from '../components/CountDown'
 import { ExperienceBar } from "../components/ExperienceBar"
 import { Profile } from '../components/Profile'
 
@@ -7,11 +9,16 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
 
       <ExperienceBar />
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <CountDown />
         </div>
         <div>
 
