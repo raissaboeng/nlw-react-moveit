3142e4ecf502a86600ad61fcb609f0a87dcf87fe
raissaboeng <raissa.boeng@gmail.com>
2021-02-25 20:38:52 -0300

    add notification browser new challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from '../../challenges.json'
 
 interface Challenge {
@@ -34,15 +34,26 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [activeChallenge, setActiveChallenge] = useState(null)
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
+
     function levelUp() {
         setLevel(level + 1)
     }
 
     function startNewChallenge() {
+
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
         const challenge = challenges[randomChallengeIndex]
 
         setActiveChallenge(challenge)
+
+        if (Notification.permission === 'granted') {
+            new Notification('move.it | Novo Desafio', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
