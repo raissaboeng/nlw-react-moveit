b85d19163dbf2cc447297440fff9433f212b9ff8
raissaboeng <raissa.boeng@gmail.com>
2021-02-25 20:29:11 -0300

    add context countdown and functions level up, complete and failed challenge with reset countdown

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,46 +1,23 @@
 import { useContext, useEffect, useState } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/CountDown.module.css'
 
-let countdownTimeout: NodeJS.Timeout;
+
 
 export function CountDown() {
-
-    const { startNewChallenge } = useContext(ChallengesContext)
-
-    const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    //Math.floor arrendonda o resultado para baixo e o minutes recebe número inteiro
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+    const {
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
+    } = useContext(CountdownContext)
 
     //transforma os minutos ou segundos em string, completa com 0 na frente caso não tenha 2 casas e separa em duas variaveis
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-    function startCountdown() {
-        setIsActive(true);
-    }
-
-    function resetCountdown() {
-        clearTimeout(countdownTimeout)
-        setIsActive(false)
-        setTime(0.1 * 60)
-    }
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000)
-        } else if (isActive && time === 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time])
 
     return (
         <div>
