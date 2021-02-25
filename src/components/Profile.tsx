ca00f31999a036d06b704c0aa9b22b6a8f0db81a
raissaboeng <raissa.boeng@gmail.com>
2021-02-25 19:54:30 -0300

    add function completeChallenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/67883555?s=460&u=e653152f8d3db3066fadb5cca035476d27dce4df&v=4" alt="Raissa Boeng" />
             <div>
                 <strong>Raissa Boeng</strong>
-                <p> <img src="icons/level.svg" alt="Level" /> Level 1</p>
+                <p> <img src="icons/level.svg" alt="Level" /> Level {level}</p>
             </div>
         </div>
     )
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,8 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
+
 }
 
 interface ChallengesProviderProps {
@@ -47,6 +49,25 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(null)
     }
 
+    function completeChallenge() {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
     return (
         <ChallengesContext.Provider
             value={{
@@ -57,7 +78,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 levelUp,
                 startNewChallenge,
                 activeChallenge,
-                resetChallenge
+                resetChallenge,
+                completeChallenge
             }}>
             {children}
         </ChallengesContext.Provider>
