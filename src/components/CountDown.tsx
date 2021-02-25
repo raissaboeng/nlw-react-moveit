69b6fa9db23cf07cb60bbf21e5dd02ed681018ac
raissaboeng <raissa.boeng@gmail.com>
2021-02-25 15:21:23 -0300

    contextChallenges and functions startNewChallenge and failed challenge button

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CountDown.module.css'
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function CountDown() {
+
+    const { startNewChallenge } = useContext(ChallengesContext)
 
     const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
@@ -35,6 +38,7 @@ export function CountDown() {
         } else if (isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
